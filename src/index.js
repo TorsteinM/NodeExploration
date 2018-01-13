5a5be5728c06e72e5320711cbd99803dcb28d0b4
TorsteinM <torsteinem@gmail.com>
2018-01-13 18:14:18 +0100

    Clearing structure for WebPack Output Management topic

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
 
-import Main from './Main';
+import { buttons } from './Main';
 
-
-
-//Handlers
-var bgChange = () => {
-    var C = num => Math.floor(Math.random()*(num+1));
-    document.body.style.backgroundColor = 'rgb('+C(255)+','+C(255)+','+C(255)+')';
-}
-
-//Register events
-var buttons = document.querySelectorAll('button');
-for (var i = 0; i < buttons.length; i++) {
-  buttons[i].onclick = bgChange;
-}
+buttons();
--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-class Main {
-    
-}
 
-export default Main
+// Function that adds some events to the buttons i the page.
+export function buttons() {
+  //The function that will be fired on the events.
+  var bgChange = () => {
+    var C = num => Math.floor(Math.random()*(num+1));
+    document.body.style.backgroundColor = 'rgb('+C(255)+','+C(255)+','+C(255)+')';
+  }
+
+  //Register events
+  var buttons = document.querySelectorAll('button');
+  var events = ['click','mouseover','blur','focus','dblclick'];
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener(events[i], bgChange);
+  }
+}
